7b9ada250e7976b1812d8b065023acdaf2eba644
thanhtung110703 <tuantranabcxyz1975@gmail.com>
2024-11-30 15:16:54 +0700

    Add files via upload

---
+++ backend/website_ivydemo.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 30, 2024 lúc 08:47 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `website_ivydemo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_brand`
+--
+
+CREATE TABLE `tbl_brand` (
+  `brand_id` int(11) NOT NULL,
+  `cartegory_id` int(11) NOT NULL,
+  `brand_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_brand`
+--
+
+INSERT INTO `tbl_brand` (`brand_id`, `cartegory_id`, `brand_name`) VALUES
+(1, 0, 'váy'),
+(2, 24, 'vio'),
+(3, 30, 'quần'),
+(8, 30, 'váy'),
+(9, 30, 'khẩu trang'),
+(13, 0, 'váy'),
+(14, 0, 'váy'),
+(15, 0, 'váy'),
+(16, 0, 'váyad'),
+(20, 27, 'váy'),
+(21, 24, 'dell'),
+(22, 29, 'vio'),
+(23, 39, 'chống khí độc');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_cartegory`
+--
+
+CREATE TABLE `tbl_cartegory` (
+  `cartegory_id` int(11) NOT NULL,
+  `cartegory_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_cartegory`
+--
+
+INSERT INTO `tbl_cartegory` (`cartegory_id`, `cartegory_name`) VALUES
+(24, 'Nữ'),
+(25, 'Nam'),
+(26, 'trẻ em'),
+(27, 'SALE'),
+(28, 'BST'),
+(39, 'khẩu trang'),
+(41, 'áo sơ mi'),
+(42, 'nuwx');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `tbl_brand`
+--
+ALTER TABLE `tbl_brand`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Chỉ mục cho bảng `tbl_cartegory`
+--
+ALTER TABLE `tbl_cartegory`
+  ADD PRIMARY KEY (`cartegory_id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_brand`
+--
+ALTER TABLE `tbl_brand`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_cartegory`
+--
+ALTER TABLE `tbl_cartegory`
+  MODIFY `cartegory_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
